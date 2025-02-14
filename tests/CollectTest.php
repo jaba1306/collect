0820c904420aa6b1d22c85875dd4e35737b00a0d
жена чонгука <kseniyadonchenko@yandex.ru>
2025-02-15 05:41:24 +0700

    gg

--- tests/CollectTest.php
+++ tests/CollectTest.php
@@ -1,13 +1,31 @@
 <?php
+use PHPUnit\Framework\TestCase; // 1
+use Collect\Collect; // 2
 
-use PHPUnit\Framework\TestCase;
-
-class CollectTest extends TestCase
+class CollectTest extends TestCase // 3
 {
-    public function testCount()
+    public function testKeysFunction() // 4
     {
-        $collect = new Collect\Collect([13,17]);
-        $this->assertSame(2, $collect->count());
+        $collection = new Collect(['name' => 'Alex', 'age' => 25, 'city' => 'Los Angeles']); // 5
+        $this->assertEquals(['name', 'age', 'city'], $collection->keys()->toArray()); // 6
     }
 
+    public function testValues() // 7
+    {
+        $collection = new Collect(['name' => 'Alex', 'age' => 25, 'city' => 'Los Angeles']); // 8
+        $this->assertEquals(['Alex', 25, 'Los Angeles'], $collection->values()->toArray()); // 9
+    }
+
+    public function testGet() // 10
+    {
+        $collection = new Collect(['name' => 'Alex', 'age' => 25, 'city' => 'Los Angeles']); // 11
+        $this->assertEquals(25, $collection->get('age')); // 12
+        $this->assertEquals(null, $collection->get('surname')); // 13
+    }
+
+    public function testExcept() // 14
+    {
+        $collection = new Collect(['name' => 'Alex', 'age' => 25, 'city' => 'Los Angeles']); // 15
+        $this->assertEquals(['name' => 'Alex', 'city' => 'Los Angeles'], $collection->except('age')->toArray()); // 16
+    }
 }
